9d82f0a3311eb6defad87452763e31ef29ee169b
24aitor <1aitorribaarchilla@gmail.com>
2016-11-18 21:46:59 +0100

    Deleted unused function of controller

--- src/Controllers/LaralangController.php
+++ src/Controllers/LaralangController.php
@@ -11,29 +11,24 @@ use Aitor24\Laralang\Models\DB_Translation;
 
 class LaralangController extends Controller
 {
-    public function retView() {
+    public function retView()
+    {
         return view('laralang::translations');
     }
 
-    public function api() {
+    public function api()
+    {
         return DB_Translation::all();
     }
 
-    public function testfunction(Request $request)
+    public function deleteTrans($id)
     {
-    if ($request->isMethod('post')){
-        return response()->json(['response' => 'This is post method']);
-    }
-
-    return response()->json(['response' => 'This is get method']);
-    }
-
-    public function deleteTrans($id) {
         $trans = DB_Translation::findOrFail($id);
         $trans->delete();
         return;
     }
-    public function editTrans($id, $translation) {
+    public function editTrans($id, $translation)
+    {
         $trans = DB_Translation::findOrFail($id);
         $trans->translation = $translation;
         $trans->touch();
