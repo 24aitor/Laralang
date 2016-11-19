f13fa76cbad2a16cdf7e534e14836c37236ee165
24aitor <1aitorribaarchilla@gmail.com>
2016-11-19 18:07:08 +0100

    Minor routes changes

--- src/Routes/web.php
+++ src/Routes/web.php
@@ -5,7 +5,10 @@ Route::group(['middleware' => 'web', 'as' => 'laralang::', 'prefix' => config('l
     Route::post('/login', 'LaralangController@login');
 
     Route::group(['middleware' => 'laralang.middleware'], function () {
-        Route::get('/', 'LaralangController@showTranslations')->name('translations');
+        Route::get('/', function () {
+            return redirect(Route('laralang::translations'));
+        })->name('home');
+        Route::get('/translations', 'LaralangController@showTranslations')->name('translations');
         Route::get('/delete/{id}', 'LaralangController@deleteTrans')->name('delete');
         Route::get('/edit/{id}/{translation}', 'LaralangController@editTrans');
         Route::get('/logout', 'LaralangController@logout')->name('logout');
