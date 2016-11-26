376356f8e3998b9ba7f9db1f63bf1d06deb6c68e
24aitor <1aitorribaarchilla@gmail.com>
2016-11-26 21:06:09 +0100

    Fixes for StyleCI

--- src/Builder/Exception.php
+++ src/Builder/Exception.php
@@ -4,13 +4,10 @@ namespace Aitor24\Laralang\Builder;
 
 class Exception extends Translation
 {
-
      /**
-      * This function do nothing, but it's neccesary to create an exception object
+      * This function do nothing, but it's neccesary to create an exception object.
       */
      public function main()
      {
-
-         return;
      }
 }
