5694925dde6395d5a9cd3c77c1f16412aa76e5e5
24aitor <1aitorribaarchilla@gmail.com>
2016-11-28 16:04:24 +0100

    Optimized code and added two new functions
    Functions added:
    - languages()
    - allLanguages()

--- src/Builder.php
+++ src/Builder.php
@@ -5,6 +5,7 @@ namespace Aitor24\Laralang;
 use Aitor24\Laralang\Builder\ApertiumTrans;
 use Aitor24\Laralang\Builder\Exception;
 use Aitor24\Laralang\Builder\MymemoryTrans;
+use Aitor24\Laralang\Models\DB_Translation;
 
 class Builder
 {
@@ -19,7 +20,7 @@ class Builder
     {
         $translator = config('laralang.default.translator');
         if (!in_array(config('laralang.default.translator'), ['apertium', 'mymemory'])) {
-            return new Exception("<font style='color:red;'>Laralang doesn't support $translator translator. Check config</font>");
+            return "<font style='color:red;'>Laralang doesn't support $translator translator. Check config</font>";
         } else {
             if (config('laralang.default.translator') == 'mymemory') {
                 return new MymemoryTrans($string);
@@ -27,5 +28,30 @@ class Builder
                 return new ApertiumTrans($string);
             }
         }
+    }
+
+    /**
+     * Get the languages used.
+     *
+     * @return array
+     */
+    public static function languages()
+    {
+        $locales = [];
+        $translations = DB_Translation::distinct()->select('to_lang')->get();
+        foreach ($translations as $object) {
+            array_push($locales, $object->to_lang);
+        }
+        return $locales;
+    }
+
+    /**
+     * Get all the available languages.
+     *
+     * @return array
+     */
+    public static function allLanguages()
+    {
+        return ['English' => 'en', 'Spanish' => 'es', 'Catalan' => 'ca', 'Portuguese' => 'pt', 'Chinese' => 'zh', 'Japanese' => 'ja', 'German' => 'de', 'French' => 'fr'];
     }
 }
