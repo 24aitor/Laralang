43836b6803c9e221df8d7db40e43f8d71dc946a0
Aitor Riba Archilla <contact@aitorriba.com>
2017-08-24 17:27:39 +0000

    Apply fixes from StyleCI

--- src/Builder/GoogleTrans.php
+++ src/Builder/GoogleTrans.php
@@ -5,31 +5,31 @@ namespace Aitor24\Laralang\Builder;
 class GoogleTrans extends Translation
 {
     /**
-      * Get translation from Google.
-      */
-     public function main()
-     {
-         $host = 'translate.googleapis.com';
+     * Get translation from Google.
+     */
+    public function main()
+    {
+        $host = 'translate.googleapis.com';
 
-         // Check if host is online.
-         if ($this->checkHost($host)) {
+        // Check if host is online.
+        if ($this->checkHost($host)) {
 
              // Host online
-             $urlString = urlencode($this->string);
-             $urldata = file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&sl=$this->from&tl=$this->to&dt=t&q=$urlString");
-             $tr = $urldata;
-             $tr = substr($tr, 3, -6);
-             $tr = $tr.'["';
-             $tr = explode('],[', $tr);
-             $trans = [];
-             foreach ($tr as $tran) {
-                 $transl = explode('","', $tran);
-                 array_push($trans, str_replace('\"', '"', ucfirst(substr($transl[0], 1))));
-             }
-             $this->translation = trim(implode(' ', $trans));
-             $this->checkSave();
+            $urlString = urlencode($this->string);
+            $urldata = file_get_contents("https://translate.googleapis.com/translate_a/single?client=gtx&sl=$this->from&tl=$this->to&dt=t&q=$urlString");
+            $tr = $urldata;
+            $tr = substr($tr, 3, -6);
+            $tr = $tr.'["';
+            $tr = explode('],[', $tr);
+            $trans = [];
+            foreach ($tr as $tran) {
+                $transl = explode('","', $tran);
+                array_push($trans, str_replace('\"', '"', ucfirst(substr($transl[0], 1))));
+            }
+            $this->translation = trim(implode(' ', $trans));
+            $this->checkSave();
 
-             return;
-         }
-     }
+            return;
+        }
+    }
 }
--- src/Builder/MymemoryTrans.php
+++ src/Builder/MymemoryTrans.php
@@ -5,47 +5,47 @@ namespace Aitor24\Laralang\Builder;
 class MymemoryTrans extends Translation
 {
     /**
-      * Get translation from mymemory API.
-      */
-     public function main()
-     {
-         $host = 'api.mymemory.translated.net';
+     * Get translation from mymemory API.
+     */
+    public function main()
+    {
+        $host = 'api.mymemory.translated.net';
 
-         // Check if host is online.
-         if ($this->checkHost($host)) {
+        // Check if host is online.
+        if ($this->checkHost($host)) {
 
              // Host online
-             $urlString = urlencode($this->string);
-             $urldata = file_get_contents("http://$host/get?q=$urlString&langpair=$this->from|$this->to");
-             $data = json_decode($urldata, true);
+            $urlString = urlencode($this->string);
+            $urldata = file_get_contents("http://$host/get?q=$urlString&langpair=$this->from|$this->to");
+            $data = json_decode($urldata, true);
 
-             if ($data['responseStatus'] != 200) {
-                 if ($this->debug == true) {
-                     if ($data['responseStatus'] == 403) {
-                         $details = ($data['responseDetails']);
-                     } else {
-                         $details = $data['responseDetails'];
-                     }
-                     $this->translation = "<font style='color:red;'>Error ".$data->responseStatus.': '.$details.'</font>';
-                 }
+            if ($data['responseStatus'] != 200) {
+                if ($this->debug == true) {
+                    if ($data['responseStatus'] == 403) {
+                        $details = ($data['responseDetails']);
+                    } else {
+                        $details = $data['responseDetails'];
+                    }
+                    $this->translation = "<font style='color:red;'>Error ".$data->responseStatus.': '.$details.'</font>';
+                }
 
-                 return;
-             }
+                return;
+            }
 
-             // Checking if any translation provides from 24aitor to use it
-             foreach ($data['matches']  as $match) {
-                 if ($match['last-updated-by'] == '24aitor') {
-                     $this->translation = $match['translation'];
-                     $this->checkSave();
+            // Checking if any translation provides from 24aitor to use it
+            foreach ($data['matches']  as $match) {
+                if ($match['last-updated-by'] == '24aitor') {
+                    $this->translation = $match['translation'];
+                    $this->checkSave();
 
-                     return;
-                 }
-             }
+                    return;
+                }
+            }
 
-             $this->translation = $data['responseData']['translatedText'];
-             $this->checkSave();
+            $this->translation = $data['responseData']['translatedText'];
+            $this->checkSave();
 
-             return;
-         }
-     }
+            return;
+        }
+    }
 }
